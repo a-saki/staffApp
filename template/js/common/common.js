bb4c5077a5184d1a01cc058aabf148a8fd12e66c
saki <manicon22@gmail.com>
2017-03-30 08:03:17 +0000

    finish template

---
+++ template/js/common/common.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var class_ts_1 = require("./class.ts");
+var person = new class_ts_1.default('stave');
+person.say();
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2pzL2NvbW1vbi9jb21tb24udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSx1Q0FBZ0M7QUFFaEMsSUFBSSxNQUFNLEdBQUcsSUFBSSxrQkFBTSxDQUFDLE9BQU8sQ0FBQyxDQUFDO0FBQ2pDLE1BQU0sQ0FBQyxHQUFHLEVBQUUsQ0FBQyIsImZpbGUiOiJqcy9jb21tb24vY29tbW9uLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFBlcnNvbiBmcm9tICcuL2NsYXNzLnRzJztcblxudmFyIHBlcnNvbiA9IG5ldyBQZXJzb24oJ3N0YXZlJyk7XG5wZXJzb24uc2F5KCk7XG5cblxuXG4iXX0=
---
+++ template/js/list.js
@@ -0,0 +1,5 @@
+$(function () {
+    $('.tabular.menu .item').tab();
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2pzL2xpc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsQ0FBQyxDQUFDO0lBRUYsQ0FBQyxDQUFDLHFCQUFxQixDQUFDLENBQUMsR0FBRyxFQUFFLENBQUM7QUFDL0IsQ0FBQyxDQUFDLENBQUMiLCJmaWxlIjoianMvbGlzdC5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZnVuY3Rpb24oKXtcbiAgXG4kKCcudGFidWxhci5tZW51IC5pdGVtJykudGFiKCk7XG59KTsiXX0=
